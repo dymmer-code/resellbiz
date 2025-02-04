85d7c86fa45ccf3248aa1000c2b6551606be474d
Manuel Rubio <manuel@altenwald.com>
2025-02-04 06:41:00 +0100

    let configure times x minute to access to Resellbiz

--- lib/resellbiz/throttle.ex
+++ lib/resellbiz/throttle.ex
@@ -8,7 +8,12 @@ defmodule Resellbiz.Throttle do
   require Logger
 
   @throttle_tries Application.compile_env(:resellbiz, :tries, 3)
-  @throttle_time_to_wait Application.compile_env(:resellbiz, :throttle_time_to_wait, 1_000)
+  @throttle_time_to_wait Application.compile_env(
+                           :resellbiz,
+                           :throttle_time_to_wait,
+                           :timer.seconds(1)
+                         )
+  @times Application.compile_env(:resellbiz, :throttle_times_per_minute, 5)
 
   defp check_throttle(tries \\ @throttle_tries)
 
@@ -18,7 +23,7 @@ defmodule Resellbiz.Throttle do
   end
 
   defp check_throttle(tries) do
-    case Hammer.check_rate("resellbiz:global", 60_000, 5) do
+    case Hammer.check_rate("resellbiz:global", :timer.minutes(1), @times) do
       {:allow, _count} ->
         :ok
 
